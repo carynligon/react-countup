5ca4b56cd25f83d572bc1b8a0fb87531874217fa
Caryn Ligon <cligon@thezebra.com>
2017-09-18 12:44:46 -0500

    cleaning up component

--- index.js
+++ index.js
@@ -1,43 +1,25 @@
 // @flow
 
-import React, { Component, Element } from 'react';
+/* eslint-disable */
+
+import React, { Component, PropTypes } from 'react';
 import Count from 'countup.js';
 
-type Props = {
-  className: string,
-  decimal: string,
-  decimals: number,
-  duration: number,
-  easingFn: () => void,
-  end: number,
-  formattingFn: () => void,
-  onComplete: () => void,
-  onStart: () => void,
-  prefix: string,
-  redraw: boolean, // eslint-disable-line react/no-unused-prop-types
-  separator: string,
-  start: number,
-  style: {},
-  suffix: string,
-  useEasing: boolean,
-  useGrouping: boolean,
-};
-
-type FormatNumberFn = (
-  start: number,
-  options: {
-    decimal: string,
-    decimals: number,
-    useGrouping: boolean,
-    separator: string,
-    prefix: string,
-    suffix: string,
-  },
-) => string;
+// type FormatNumberFn = (
+//   start: number,
+//   options: {
+//     decimal: string,
+//     decimals: number,
+//     useGrouping: boolean,
+//     separator: string,
+//     prefix: string,
+//     suffix: string,
+//   },
+// ) => string;
 
 // Adapted from the countup.js format number function
 // https://github.com/inorganik/countUp.js/blob/master/countUp.js#L46-L60
-export const formatNumber: FormatNumberFn = (start, options) => {
+export const formatNumber = (start, options) => {
   const num = `${start.toFixed(options.decimals)}`;
   const x = num.split('.');
   let x1 = x[0];
@@ -52,7 +34,7 @@ export const formatNumber: FormatNumberFn = (start, options) => {
   return `${options.prefix}${x1}${x2}${options.suffix}`;
 };
 
-export const startAnimation = (component: Component<*, *, *>) => {
+export const startAnimation = component => {
   if (!(component && component.spanElement)) {
     throw new Error(
       'You need to pass the CountUp component as an argument!\neg. this.myCountUp.startAnimation(this.myCountUp);',
@@ -74,7 +56,7 @@ export const startAnimation = (component: Component<*, *, *>) => {
     suffix,
     useEasing,
     useGrouping,
-  }: Props = component.props;
+  } = component.props;
 
   const countupInstance = new Count(
     component.spanElement,
@@ -102,31 +84,11 @@ export const startAnimation = (component: Component<*, *, *>) => {
 };
 
 export default class CountUp extends Component {
-  static defaultProps = {
-    className: undefined,
-    decimal: '.',
-    decimals: 0,
-    duration: 3,
-    easingFn: null,
-    end: 100,
-    formattingFn: null,
-    onComplete: undefined,
-    onStart: undefined,
-    prefix: '',
-    separator: ',',
-    start: 0,
-    suffix: '',
-    redraw: false,
-    style: undefined,
-    useEasing: true,
-    useGrouping: false,
-  };
-
   componentDidMount() {
     startAnimation(this);
   }
 
-  shouldComponentUpdate(nextProps: Props) {
+  shouldComponentUpdate(nextProps) {
     const hasCertainPropsChanged =
       this.props.duration !== nextProps.duration ||
       this.props.end !== nextProps.end ||
@@ -139,25 +101,21 @@ export default class CountUp extends Component {
     startAnimation(this);
   }
 
-  spanElement = null;
-
-  refSpan = (span: Element<*>) => {
+  refSpan(span) {
     this.spanElement = span;
-  };
-
-  props: Props;
+  }
 
   render() {
     const {
-      className,
-      start,
-      style,
-      decimal,
-      decimals,
-      useGrouping,
-      separator,
-      prefix,
-      suffix,
+      className = undefined,
+      start = 0,
+      style = undefined,
+      decimal = '.',
+      decimals = 0,
+      useGrouping = false,
+      separator = ',',
+      prefix = '',
+      suffix = '',
     } = this.props;
 
     return (
@@ -174,3 +132,23 @@ export default class CountUp extends Component {
     );
   }
 }
+
+CountUp.propTypes = {
+  className: PropTypes.string,
+  decimal: PropTypes.string,
+  decimals: PropTypes.number,
+  duration: PropTypes.number,
+  easingFn: PropTypes.func,
+  end: PropTypes.number,
+  formattingFn: PropTypes.func,
+  onComplete: PropTypes.func,
+  onStart: PropTypes.func,
+  prefix: PropTypes.string,
+  redraw: PropTypes.bool, // eslint-disable-line react/no-unused-prop-types
+  separator: PropTypes.string,
+  start: PropTypes.number,
+  style: PropTypes.object,
+  suffix: PropTypes.string,
+  useEasing: PropTypes.bool,
+  useGrouping: PropTypes.bool,
+};
